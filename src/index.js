cdae2705e3296a2c92c0e0da5e408d7b4deb8ecf
rebeccanewborn <rjn58@cornell.edu>
2017-11-19 11:26:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
-ReactDOM.render(
-  meInReact,
-  document.getElementById('global')
-);
+let li1 = React.createElement("li", {}, "JavaScript");
+let li2 = React.createElement("li", {}, "React");
+let li3 = React.createElement("li", {}, "Movies");
+let li4 = React.createElement("li", {}, "Ice cream");
 
-export default meInReact
-*/
+let ul = React.createElement("ul", { className: "my-interests" }, [
+  li1,
+  li2,
+  li3,
+  li4
+]);
+
+let h1 = React.createElement("h1", {}, "An Awesome Person");
+let p = React.createElement("p", {}, "Who is learning React");
+
+let meInReact = React.createElement("div", { className: "me" }, [h1, p, ul]);
+
+ReactDOM.render(meInReact, document.getElementById("global"));
+
+export default meInReact;
